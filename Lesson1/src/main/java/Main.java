145c65d2a597f785a2665f8092d7e7f89354e280
Kolade Joseph Ayo-Vaughan <joey.ayov@gmail.com>
2015-10-26 09:57:23 -0400

    added .gitignore

---
+++ Lesson1/src/main/java/Main.java
@@ -0,0 +1,13 @@
+/**
+ * Created by joeyvon.
+ * 10/26/15
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+        int numA; // should be 7
+        int numB; // should be 13
+        int numC; // should be 3
+    }
+}
---
+++ Lesson1/src/main/java/MathPerformer.java
@@ -0,0 +1,16 @@
+/**
+ * Created by joeyvon.
+ * 10/26/15
+ */
+
+public class MathPerformer {
+    private static int number = 0;
+
+    public int addToNum(int num) {
+        return number + num;
+    }
+
+    public int subtractFromNumber(int num) {
+        return number - num;
+    }
+}
